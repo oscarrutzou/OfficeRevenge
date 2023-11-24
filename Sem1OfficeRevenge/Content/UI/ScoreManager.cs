0aab8ea214a7f36c3c9e1fa9dd50bf4627ec5617
leonardzz7 <leonard@leooli.dk>
2023-11-24 14:16:19 +0100

    Score Manager n Shieet

---
+++ Sem1OfficeRevenge/Content/UI/ScoreManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct2D1.Effects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sem1OfficeRevenge.Content.UI
+{
+    static internal class ScoreManager
+    {
+        public static int killCount = 0;
+
+        public static void UpdateScore()
+        {
+            string appdataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string folder = Path.Combine(appdataPath, "OfficeRevengeData");
+            Directory.CreateDirectory(folder);
+            string path = Path.Combine(folder, "data.txt");
+            FileStream stream = File.Open(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            try
+            {
+                StreamReader reader = new StreamReader(stream);
+                int temp; 
+                int.TryParse(reader.ReadToEnd(), out temp);
+                if (temp > killCount)
+                {
+                    killCount = temp;
+                }
+                else
+                {
+                    stream.SetLength(0);
+                    byte[] info = new UTF8Encoding(true).GetBytes(killCount.ToString());
+                    stream.Write(info, 0, info.Length);
+                }
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+        public static void Draw(Vector2 pos) 
+        {
+            Global.spriteBatch.DrawString(GlobalTextures.defaultFont, "Kill Count: " + killCount, pos, Color.Red);
+
+        }
+
+
+    }
+}
--- Sem1OfficeRevenge/Scenes/TestSceneLeonard.cs
+++ Sem1OfficeRevenge/Scenes/TestSceneLeonard.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using Sem1OfficeRevenge.Content.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +12,7 @@ namespace Sem1OfficeRevenge
     public class TestSceneLeonard : Scene
     {
         CombatEnemy comEnm;
+        bool pressed;
         public TestSceneLeonard()
         {
             
@@ -22,11 +26,23 @@ namespace Sem1OfficeRevenge
 
         public override void DrawInWorld()
         {
+            ScoreManager.Draw(new Vector2(10, 10));
             base.DrawInWorld();
         }
 
         public override void Update()
         {
+            ScoreManager.UpdateScore();
+            if (Keyboard.GetState().IsKeyDown(Keys.K))
+            {
+                
+                
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.I))
+            {
+                ScoreManager.killCount++;
+            }
             comEnm.Update();
             base.Update();
         }
