2eaaa487719307935101cc84dadb1ed10b56a10b
oscarrutzou <91065000+oscarrutzou@users.noreply.github.com>
2023-11-25 18:11:39 +0100

    Fake loading screen
    Loader men venter også 30ms mellem hver load så man kan se vores flotte loading screen lol

--- Sem1OfficeRevenge/Scenes/LoadingScene.cs
+++ Sem1OfficeRevenge/Scenes/LoadingScene.cs
@@ -1,16 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Sem1OfficeRevenge;
+using System;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
-namespace Sem1OfficeRevenge
+public class LoadingScene : Scene
 {
-    public class LoadingScene : Scene
-    {
-        public override void Initialize()
-        {
+    private bool isLoading = false;
+    //private float progress = 0f;
 
+    public override void Initialize()
+    {
+        // Start loading content in a separate task
+        Task.Run(() => LoadContent()).ContinueWith(t => OnContentLoaded());
+        isLoading = true;
+    }
+
+    private void LoadContent()
+    {
+        // Load your content here
+        GlobalAnimations.LoadContent();
+    }
+
+    private void OnContentLoaded()
+    {
+        // Switch to the main menu when the content is loaded
+        //Global.world.ChangeScene(Scenes.MainMenu);
+        //isLoading = false;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        // Optionally, update your loading screen animation here
+        if (isLoading)
+        {
+            // Update loading screen animation
+        }
+    }
+
+    public override void DrawOnScreen()
+    {
+        base.DrawOnScreen();
+        // Draw your loading screen here
+        if (isLoading)
+        {
+            // Draw loading screen
+            Vector2 pos = Global.world.camera.Center + new Vector2(0, -300);
+            Global.spriteBatch.DrawString(GlobalTextures.defaultFont, $"Loading: {GlobalAnimations.progress * 100}%", pos, Color.Black);
         }
     }
 }
--- Sem1OfficeRevenge/World/GameWorld.cs
+++ Sem1OfficeRevenge/World/GameWorld.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
@@ -9,7 +10,6 @@ namespace Sem1OfficeRevenge
     public class GameWorld : Game
     {
         private Dictionary<Scenes, Scene> scenes = new Dictionary<Scenes, Scene>();
-        private int activeSceneIndex;
         public Camera camera;
         public GameWorld()
         {
@@ -25,7 +25,7 @@ namespace Sem1OfficeRevenge
         {
             WindowedScreen();
             GlobalTextures.LoadContent();
-            GlobalAnimations.LoadContent();
+            //GlobalAnimations.LoadContent();
 
             //camera = new Camera(new Vector2(Global.graphics.PreferredBackBufferWidth / 2, Global.graphics.PreferredBackBufferHeight / 2));
             camera = new Camera(Vector2.Zero);
@@ -33,9 +33,7 @@ namespace Sem1OfficeRevenge
             
             GenerateScenes();
             ChangeScene(Scenes.MainMenu);
-
             
-
             base.Initialize();
         }
 
